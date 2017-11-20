7a1427ce47b0117bb35f12733db97cd00c9d73ad
Rui de Alencar Frota <31742217+ruidealencar@users.noreply.github.com>
2017-11-20 11:00:41 -0200

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -6,33 +6,47 @@ public class Principal
 
 	public static void main(String[] args)
 	{
+	
 		Scanner scan = new Scanner (System.in);
 		Random n = new Random();
+		String resposta = "s";
 		
-		
+	
 		System.out.println("********************************************************************");
 		System.out.println("***************************JOGO DE DADOS****************************");
 		System.out.println("********************************************************************");
-		System.out.println("VAMOS JOGAR UM DADO E VOCÊ VAI DIGITAR SEU PALPITE DE 0 - 6 É CLARO ");
+		System.out.println("VAMOS JOGAR UM DADO E VOCÃŠ VAI DIGITAR SEU PALPITE DE 1 - 6 Ã‰ CLARO ");
+	do{
 		System.out.println("Qual seu Palpite? ");
 		int palpite = scan.nextInt();
-		System.out.println("Palpite = "+palpite);
+		
 		int resultado = n.nextInt(6)+1;
 		System.out.println("Resultado = "+resultado);
-		
-		
-		
 		if (palpite == resultado)
 		{
-			System.out.println("PARABÉS VOCÊ GANHOU : )");
+			System.out.println("********************************************************************");
+			System.out.println("************************PARABÃ‰NS VOCÃŠ GANHOU************************");
+			System.out.println("********************************************************************");
+			System.out.println(" ");
+			System.out.println("Deseja continuar?(s/n)");
 			
 		}else
 		{
-			System.out.println("INFELIZMENTE VOCÊ PERDEU : (");
+			System.out.println("********************************************************************");
+			System.out.println("**********************INFELIZMENTE VOCÃŠ PERDEU**********************");
+			System.out.println("********************************************************************");
+			System.out.println(" ");
+			System.out.println("Deseja continuar?(s/n)");
+			
+	
 		}
-		
-		scan.close();	
-
-	}
+		resposta = scan.next();
+	}while(resposta.equals("s"));
+		scan.close();
+		System.out.println("********************************************************************");
+		System.out.println("****************************FIM DE JOGO*****************************");
+		System.out.println("********************************************************************");
+	  }
+	
 
 }
